f38c9f3faa3cbfdf637c8e5084891645abf8dd43
chukwudi <nwaforcs@edutams.net>
2026-03-18 12:11:49 +0100

    adding auto commit to github graph

--- index.js
+++ index.js
@@ -1,40 +1,47 @@
-import jsonfile from "jsonfile";
+import fs from "fs";
 import moment from "moment";
 import simpleGit from "simple-git";
-import random from "random";
 
 const path = "./data.json";
+const git = simpleGit();
 
-const markCommit = (x, y) => {
-  const date = moment()
-    .subtract(1, "y")
-    .add(1, "d")
-    .add(x, "w")
-    .add(y, "d")
-    .format();
+const startYear = 2020;
+const endYear = 2026;
 
-  const data = {
-    date: date,
-  };
-
-  jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date }).push();
-  });
+// Helper: get all dates in a year
+const getDatesInYear = (year) => {
+  let dates = [];
+  for (let month = 0; month < 12; month++) {
+    const daysInMonth = moment({ year, month }).daysInMonth();
+    for (let day = 1; day <= daysInMonth; day++) {
+      dates.push(moment({ year, month, day }).format());
+    }
+  }
+  return dates;
 };
 
-const makeCommits = (n) => {
-  if(n===0) return simpleGit().push();
-  const x = random.int(0, 54);
-  const y = random.int(0, 6);
-  const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+// Commit all dates for a given year
+const commitYear = async (year) => {
+  console.log(`\n📅 Pushing commits for ${year}...`);
+  const dates = getDatesInYear(year);
 
-  const data = {
-    date: date,
-  };
-  console.log(date);
-  jsonfile.writeFile(path, data, () => {
-    simpleGit().add([path]).commit(date, { "--date": date },makeCommits.bind(this,--n));
-  });
+  for (let date of dates) {
+    fs.writeFileSync(path, JSON.stringify({ date }, null, 2));
+    await git.add([path]);
+    await git.commit(date, { "--date": date });
+  }
+
+  // Push after finishing the year
+  await git.push();
+  console.log(`✅ Finished pushing commits for ${year}`);
 };
 
-makeCommits(100);
+// Main: loop through each year
+const pushAllYears = async () => {
+  for (let year = startYear; year <= endYear; year++) {
+    await commitYear(year);
+  }
+  console.log("\n🎉 All commits pushed from 2020 to 2026!");
+};
+
+pushAllYears().catch(console.error);
